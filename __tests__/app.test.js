0fd95810a6179b6deb38049d4927a2ad0b0492c1
Ben <b.bax23@hotmail.co.uk>
2022-09-05 15:00:45 +0100

    add first test and model function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,3 +6,24 @@ const seed = require('../db/seeds');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe('GET /api/categories', () => {
+  test('should return an array of objects with slug and description properties', () => {
+    return request(app)
+      .get('/api/categories')
+      .expect(200)
+      .then(({ body }) => {
+        const { games } = body;
+        expect(games).toBeInstanceOf(Array);
+        expect(games.length > 0).toBe(true);
+        games.forEach((game) => {
+          expect(game).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
