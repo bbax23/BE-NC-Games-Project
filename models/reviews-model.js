24175195bd18483a89fec4723264a94e02898548
Ben <b.bax23@hotmail.co.uk>
2022-09-08 16:16:28 +0100

    add updated query to join reviews and comments to get comment count

--- models/reviews-model.js
+++ models/reviews-model.js
@@ -1,17 +1,16 @@
 const db = require('../db/connection');
-
 exports.selectRevById = (review_id) => {
-  return db
-    .query('SELECT * FROM reviews WHERE review_id = $1;', [review_id])
-    .then((result) => {
-      if (result.rowCount === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: 'That review id does not exist',
-        });
-      }
-      return result.rows[0];
-    });
+  const queryString =
+    'SELECT reviews.*, COUNT(comment_id) AS comment_count FROM reviews LEFT JOIN comments ON comments.review_id = reviews.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id;';
+  return db.query(queryString, [review_id]).then((result) => {
+    if (result.rowCount === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: 'That review id does not exist',
+      });
+    }
+    return result.rows[0];
+  });
 };
 
 exports.updateReviewVotes = (voteObj, review_id) => {
